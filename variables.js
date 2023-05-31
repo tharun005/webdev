7c215cd1a383f4b897ff906d1ca2aa3d3b0ac559
tharun <tharunchoudary09@gmail.com>
2023-05-31 15:08:36 +0530

    JS variable short notes

--- variables.js
+++ variables.js
@@ -1,1 +1,15 @@
-const myfirstVariable =300;
+const myfirstVariable =300;// variables cannot be changed with const
+//const can neither redeclared nor reassined
+//the scope of const varialbe is block or function level
+
+let myvar=200//let variables cannot be redeclared but can be reassigned
+//example
+ myvar=100//this is possible with let
+ let myvar=500//thros you out an error
+ //the scope of let variable is block level
+
+ //VAR VARIABLE
+/*it has global scope redeclaration and reassignement is possible*/
+ var varia=10
+  varia=20
+  var varia=30// example of redeclartion and reassignment 
